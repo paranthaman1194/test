59ea12332a4b7f76aab4f1ae4b616c825bf0b8be
paranthaman1194 <paranand1194@gmail.com>
2025-08-07 01:16:02 +0530

    test

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "test"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
